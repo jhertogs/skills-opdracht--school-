e6cce1d1fb134bb4e984241de0d0d82ee30391e2
Josse Hertogs <jossehertogs@gmail.com>
2024-09-23 10:56:05 +0200

    now have array with dates and array with all the data

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 
 import re
 fileArray = []
-pattern = "^260,\d{4}-\d{2}-\d{2}$" # regex for YYYY-MM-DD https://blog.finxter.com/regex-match-dates/ not my code lol, im too stupid for regex
+datarr = []
+pattern = r"^260,(\d{8})" # regex for YYYY-MM-DD https://blog.finxter.com/regex-match-dates/ not my code lol, im too stupid for regex
 
 
 with open("knmi.txt") as file:
     for x in file:
         x = x.strip()
         if (x.startswith("260")):
+          
           fileArray.append(x)
 
 
-   
+for i in range(len(fileArray)):
+    match = re.match(pattern, fileArray[i])
+    if match:
+       date = match.group(1)
+       datarr.append(date)
 
+#for i in range(len(fileArray)):
 
+#print(fileArray)
+print(datarr)
 
-print(fileArray)
