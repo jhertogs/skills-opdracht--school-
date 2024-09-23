9f112687bdf323342f7337499504d90a02177387
Josse Hertogs <jossehertogs@gmail.com>
2024-09-23 10:28:51 +0200

    trying regex patterns

---
+++ main.py
@@ -0,0 +1,18 @@
+
+import re
+fileArray = []
+pattern = "^\d{4}-\d{2}-\d{2}$" # regex for YYYY-MM-DD https://blog.finxter.com/regex-match-dates/ not my code lol, im too stupid for regex
+
+
+with open("knmi.txt") as file:
+    for x in file:
+        x = x.strip()
+        if (x.startswith("260")):
+          fileArray.append(x)
+
+
+   
+
+
+if re.match(pattern, )
+print(fileArray)
