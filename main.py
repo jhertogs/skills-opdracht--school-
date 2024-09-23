1fa726516e3462f12b802356399663fcd2db6cb5
Josse Hertogs <jossehertogs@gmail.com>
2024-09-23 11:46:17 +0200

    date and data now get displayed more nicely

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import re
 from datetime import datetime
 import csv
 
-
-
 fileArray = []
 dateArr = []
+date_data_array = []
 pattern = r"^260,(\d{8})" # regex for YYYYMMDD https://blog.finxter.com/regex-match-dates/ not my code lol, im too stupid for regex
 
 
@@ -27,17 +26,15 @@ for i in range(len(fileArray)):
 
 trueDates = [datetime.strptime(date, "%Y%m%d" ) for date in dateArr]
 
-date_data_array = []
 minimum_len  = min(len(fileArray), len(trueDates))
 
 for n in range(minimum_len):
-   
    date_data_array.append([trueDates[n], fileArray[n]])
    
 
 with open("date_data.csv", "w", newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(["Date", "         HH,   DD,   FH,   FF,   FX,    T, T10N,   TD,   SQ,    Q,   DR,   RH,    P,   VV,    N,    U,   WW,   IX,    M,    R,    S,    O,    Y"])
+    csv_writer.writerow(["Date", "         HH    DD    FH    FF    FX     T  T10N    TD    SQ     Q    DR    RH     P    VV     N     U    WW    IX     M     R     S     O     Y"])
 
     for k in date_data_array:
         csv_writer.writerow([k[0].strftime('%Y-%m-%d'), k[1]])  
